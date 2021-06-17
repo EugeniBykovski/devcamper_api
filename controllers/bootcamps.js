bc151a667ebba24509279faf19566bc11627b9b9
EugeniBykovski <bykovskieugeni@mail.ru>
2021-06-17 11:59:03 +0300

    added COurse model, routes and controllers. added functionality for add, update and remove courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr))
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses')
 
   // Select fields
   if(req.query.select) {
@@ -118,7 +118,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route DELETE /api/v1/bootcamps/:id
 // @access Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+  const bootcamp = await Bootcamp.findById(req.params.id)
 
   if(!bootcamp) {
     return next(
@@ -126,11 +126,13 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     )
   }
 
+  bootcamp.remove()
+
   res.status(200).json({ success: true, data: {} })
 })
 
 
-// @desc Get bootcamps within a redius
+// @desc Get bootcamps within   a redius
 // @route GET /api/v1/bootcamps/radius/:zipcode/:distance
 // @access Private
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,6 +99,10 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now
     }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
 );
 
@@ -127,6 +131,21 @@ BootcampSchema.pre('save', async function(next) {
   this.address = undefined
 
   next()
+})
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id })
+  next()
+})
+
+// Reverse populate with Virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
 })
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)

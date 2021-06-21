36e681359b6de3a81edae5729739a526ea0c9382
EugeniBykovski <bykovskieugeni@mail.ru>
2021-06-21 11:59:56 +0300

    added ownership configurations, forgot, reset and update password functionality

--- models/Course.js
+++ models/Course.js
@@ -54,6 +54,11 @@ const CourseSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'Bootcamp',
     required: true
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
   }
 })
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require('express')
-const { register, login, getMe } = require('../controllers/auth')
+const { 
+  register, 
+  login,
+  getMe, 
+  forgotPassword, 
+  resetPassword,
+  updateDetails,
+  updatePassword
+} = require('../controllers/auth')
 
 const router = express.Router()
 
@@ -14,5 +22,17 @@ router
 router
   .get('/me', protect, getMe)
 
-module.exports = router
+router
+  .put('/updatedetails', protect, updateDetails)
 
+router
+  .put('/updatepassword', protect, updatePassword)
+
+router
+  .post('/forgotpassword', forgotPassword)
+
+router
+  .put('/resetpassword/:resettoken', resetPassword)
+
+module.exports = router
+ 
